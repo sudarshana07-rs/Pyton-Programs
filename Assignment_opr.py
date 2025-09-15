43be4254a7199e3124a4c577682c4d1803e20e28
sudarshana07-rs <sudarshana.rs07@gmail.com>
2025-09-15 12:24:00 +0530

    changes

---
+++ Assignment_opr.py
@@ -0,0 +1,45 @@
+# =(Assignment)
+a=3
+print(a)
+
+# +=
+a=10
+b=20
+a+=b # a=a+b
+print(a)
+
+# -=
+a=30
+b=20
+a-=b # a=a-b
+print(a)
+
+# *=
+a=10
+b=20
+a*=b # a=a*b
+print(a)
+
+# /=
+a=10
+b=5
+a/=b # a=a/b
+print(a)
+
+# //=
+a=10
+b=3
+a//=b # a=a//b
+print(a)
+
+# %=
+a=10
+b=3
+a%=b # a=a%b
+print(a)
+
+# **=
+a=10
+b=20
+a**=b # a=a**b
+print(a)
