6a34a470edd6e650cd9bb395d06748a39cbad0bd
sudarshana07-rs <sudarshana.rs07@gmail.com>
2025-09-15 12:02:04 +0530

    changes

---
+++ Arithmetic_opr.py
@@ -0,0 +1,41 @@
+#Addition operator
+a=10
+b=20
+add=a+b
+print(add)
+
+#Subtraction operator
+a=20
+b=10
+subtraction=a-b
+print(subtraction)
+
+#Multiplication
+a=2
+b=4
+multiplication=a*b
+print(multiplication)
+
+#Division
+a=4
+b=2
+division=a/b
+print(division)
+
+#Floor Division
+a=10
+b=3
+floor=a//b
+print(floor)
+
+#Modulo
+a=10
+b=5
+modulo=a%b
+print(modulo)
+
+#Exponential
+a=2
+b=3
+exponent=a**b
+print(exponent)
