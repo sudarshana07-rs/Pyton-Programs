159db7e2ebc48cd93b1331a4cb50f1031797894e
sudarshana07-rs <sudarshana.rs07@gmail.com>
2025-09-15 12:35:26 +0530

    changes

---
+++ Comparison_opr.py
@@ -0,0 +1,35 @@
+# ==
+a=10
+b=10
+c= a==b
+print(c)
+
+# !=
+a=10
+b=10
+c= a!=b
+print(c)
+
+# >
+a=10
+b=3
+c= a>b
+print(c)
+
+# <
+a=10
+b=40
+c= a<b
+print(c)
+
+# >=
+a=10
+b=5
+c= a>=b
+print(c)
+
+# <=
+a=10
+b=50
+c= a<=b
+print(c) 
