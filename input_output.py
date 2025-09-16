f50d59aa9055bc43fcb9129e871aea2ea26f1dc7
sudarshana07-rs <sudarshana.rs07@gmail.com>
2025-09-16 17:37:20 +0530

    changes

---
+++ input_output.py
@@ -0,0 +1,28 @@
+# Input and Output Statements
+
+#Example 1
+# name=input("Enter your name: ")
+# print("Hello",name,sep=",",end="!")
+
+#Example 2
+# num=int(input("Enter a number: "))
+# print("You entered",num,sep=": ")
+
+#Example 3
+# num=float(input("Enter the value of Pi: "))
+# print("Value of Pi:",num,sep="")
+
+#Example 4
+# a=input("Enter three numbers: ")
+# x,y,z= a.split(" ")
+# sum=int(x)+int(y)+int(z)
+# print(sum)
+
+#Example 5
+# a=input("Enter your name and age together:")
+# name,age= a.split(",")
+# print("Your name:",name,",Your age:",age,sep="")
+
+#Example 6
+a=input("Enter 5: ")
+print("Count down",a,end=",4,3,2,1 Blast off!!!")
