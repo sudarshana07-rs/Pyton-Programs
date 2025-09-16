df6e5e4ecf2266979abbd81311ef819b0e2e8a51
sudarshana07-rs <sudarshana.rs07@gmail.com>
2025-09-16 18:32:22 +0530

    changes

--- Basic_problem1.py
+++ Basic_problem1.py
@@ -1,2 +1,49 @@
 # Basic Problem Solving 1
-# Add
+# Add teo numbers
+# a,b=(input("Enter two numbers: ")).split(" ")
+# sum=int(a)+ int(b)
+# print(f"Sum: {sum}")
+
+# a=int(input("Enter a value:"))
+# b=int(input("Enter b value: "))
+# sum=a+b
+# print(f"Sum: {sum}")
+
+#Area of a circle
+# radius=int(input("Enter the radius of the circle: "))
+# a=3.14*radius*radius
+# print(f"Area of circle:{a}")
+
+#Find Roots
+# a=int(input("Give a: "))
+# b=int(input("Give b: "))
+# c=int(input("Give c: "))
+# d=b**2-(4*a*c)
+# r1=(-b+d**0.5)/2*a
+# r2=(-b-d**0.5)/2*a
+# print(f"Root 1= {r1} and Root 2= {r2}")
+
+# Swaping two number with temp variable
+# a=int(input("Enter a number:"))
+# b=int(input("Enter a number: "))
+# print(f"before swap:{a,b}")
+# temp=a
+# a=b
+# b=temp
+# print(f"After swap:{a,b}")
+
+#Swaping two numbers without using temp variable
+# a=int(input("Enter a number: "))
+# b=int(input("Enter a number: "))
+# print(f"Before swap: {a,b}")
+# a+=b
+# b=a-b
+# a-=b
+# print(f"After swap: {a,b}")
+
+# Converting temperatures
+# c=int(input("Enter the temperature in Celsius: "))
+# f=(c*9/5)+32
+# k=273+c
+# print(f"Fahrenheit: {f}")
+# print(f"Kalvin: {k}")
